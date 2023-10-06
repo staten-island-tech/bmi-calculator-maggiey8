ee03dd1c1a2686b1d7dd976e1952dd06782f22f5
maggiey8 <144460257+maggiey8@users.noreply.github.com>
2023-10-06 10:15:22 -0400

    Update app.js

--- app.js
+++ app.js
@@ -7,31 +7,31 @@ function getBMI(x, y) {
     console.log(BMI)
 }
 
+getBMI(height, weight)
+
 function checkBMI(z) {
     if (z < 18.5) {
         console.log("Underweight");
     }
-    else if (18.5 <= z && z < 25) {
+    else if (z < 25) {
         console.log("Healthy Weight");
     }
-    else if (25 <= z && z < 30) {
+    else if (z < 30) {
         console.log("Overweight");
     }
-    else if (z >= 30) {
-        console.log("Obesity");
-    }
     else {
-        console.log("error")
+        console.log("Obesity");
     }
 }
     
+checkBMI((y/((x*x)/10000)));
 
-function init() {
+/* function init() {
 //get value for weight and height
 //get bmi 
     let BMI = Number(getBMI(height, weight));
-    checkBMI(BMI);
+
 }
 
 console.log(user)
-init();
+init(); */
